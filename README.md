0d701fb00ae33c7a10bc13498541f1952e9c0b8f
Lukas Werner <me@lukaswerner.com>
2024-07-21 17:44:09 -0700

    feat: get loading crsqlite working in go

---
+++ README.md
@@ -0,0 +1,18 @@
+# sqlite-sync-demo
+
+This is the source code for the accompanying article.
+
+## Installation
+
+#### Prerequisites
+
+1. Go
+2. Working GCC compiler (for sqlite3)
+
+---
+
+1. Grab yourself a copy of
+   [cr-sqlite](https://github.com/vlcn-io/cr-sqlite/releases/latest) and install
+   the library.
+2. Download and build the source code
+3. Setup a syncing folder on `todo-sync`
--- main.go
+++ main.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"database/sql"
-	_ "github.com/mattn/go-sqlite3"
 	"log"
+
+	"github.com/mattn/go-sqlite3"
 )
 
 func mustNot(err error) {
@@ -13,12 +14,23 @@ func mustNot(err error) {
 }
 
 func main() {
-	db, err := sql.Open("sqlite3", "./todos.db")
-	mustNot(err)
-	defer db.Close()
+	sql.Register("cr-sqlite", &sqlite3.SQLiteDriver{
+		Extensions: []string{"crsqlite"},
+	})
 
-	_, err = db.Exec(`CREATE TABLE todos (id PRIMARY KEY NOT NULL, description, completed);`)
+	db, err := sql.Open("cr-sqlite", "./todos.db")
 	mustNot(err)
+	defer func(db *sql.DB) {
+		db.Exec(`select crsql_finalize();`) // clean up after cr-sqlite
+		db.Close()
+	}(db)
+
+	_, err = db.Exec(`CREATE TABLE todos (id PRIMARY KEY NOT NULL, description, completed);`) // make our todos table
+	mustNot(err)
+	_, err = db.Exec(`select crsql_as_crr('todos');`) //make it CRDT
+	mustNot(err)
+
+	// Lets make some toast!
 	_, err = db.Exec(`INSERT INTO todos (id, description, completed) VALUES (?, ?, ?);`, 1, "Get Toast", true)
 	mustNot(err)
 	_, err = db.Exec(`INSERT INTO todos (id, description, completed) VALUES (?, ?, ?);`, 2, "Get Butter", false)
